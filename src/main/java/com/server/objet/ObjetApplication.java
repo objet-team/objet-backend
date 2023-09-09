9aa9d103ca6b8857f7fd59d6c60b8346528ccb06
k0000k <locker50@naver.com>
2023-09-10 00:51:25 +0900

    feat: create project

---
+++ src/main/java/com/server/objet/ObjetApplication.java
@@ -0,0 +1,13 @@
+package com.server.objet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/objet/ObjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.objet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
