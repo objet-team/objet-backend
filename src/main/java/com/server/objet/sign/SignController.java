180ee524f7adbbfb0de705200b1a5bc4db969308
k0000k <locker50@naver.com>
2023-09-10 01:35:19 +0900

    feat: add baseline

---
+++ src/main/java/com/server/objet/sign/SignController.java
@@ -0,0 +1,16 @@
+package com.server.objet.sign;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("sign")
+public class SignController {
+
+    @GetMapping("/test")
+    public String tester() {
+        return "Success";
+    }
+
+}
---
+++ src/main/java/com/server/objet/sign/SignService.java
@@ -0,0 +1,7 @@
+package com.server.objet.sign;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignService {
+}
