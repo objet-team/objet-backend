7034e97b7fd980b922862a188e2a1e2ffba50dfb
k0000k <locker50@naver.com>
2023-09-10 11:37:09 +0900

    feat: feat update baseline

---
+++ src/main/java/com/server/objet/global/entity/User.java
@@ -0,0 +1,7 @@
+package com.server.objet.global.entity;
+
+public class User {
+    /*
+    * 엔티티들이 모여있는 패키지입니다. 아직 디펜던시에 JPA는 추가해두지 않은 상태입니다.
+    * */
+}
---
+++ src/main/java/com/server/objet/global/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.server.objet.global.repository;
+
+public interface UserRepository {
+    /*
+    * 레포지토리들이 모여있는 패키지입니다. 디펜던시에 JPA를 추가한 후 작업해주세요.
+    * */
+}
