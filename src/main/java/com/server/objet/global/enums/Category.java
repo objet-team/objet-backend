9d97aa70014580b804817e24b9c31c9684377dc8
yujin <jini1514@soongsil.ac.kr>
2023-09-21 21:37:53 +0900

    feat: 카테고리 enums

--- src/main/java/com/server/objet/global/enums/Category.java
+++ src/main/java/com/server/objet/global/enums/Category.java
@@ -1,5 +1,22 @@
 package com.server.objet.global.enums;
 
 public enum Category {
-    UIUX;
+    UX_UI("UX/UI"),
+    GRAPHIC_DESIGN("그래픽 디자인"),
+    VIDEO_AND_MOTION_GRAPHICS("영상&모션 그래픽"),
+    ILLUSTRATION("일러스트레이션"),
+    CHARACTER_DESIGN("캐릭터 디자인"),
+    PHOTOGRAPHY("사진"),
+    CRAFTS("공예"),
+    CERAMICS_AND_GLASS("도예/유리");
+
+    private final String displayName;
+
+    Category(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
