c1aa48a446c3bfb16d53f5b99c28e2f5c66dc0da
k0000k <locker50@naver.com>
2023-09-25 02:45:45 +0900

    fix: Content entity

---
+++ src/main/java/com/server/objet/global/entity/Content.java
@@ -0,0 +1,39 @@
+package com.server.objet.global.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "content")
+public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_id")
+    private Long id;
+
+    @Column(name = "p_id")
+    private Long productId;
+
+    @Column(name = "content_type")
+    private String type;
+
+    @Column(name = "content_order")
+    private Integer order;
+
+    @Column(name = "size_type")
+    private String sizeType;
+
+    @Column(name = "desc")
+    private String description;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "align")
+    private String align;
+
+}
---
+++ src/main/java/com/server/objet/global/repository/ContentRepository.java
@@ -0,0 +1,10 @@
+package com.server.objet.global.repository;
+
+import com.server.objet.global.entity.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ContentRepository extends JpaRepository<Content, Long> {
+    Optional<Content> findByProductIdAndOrder(Long productId, Integer order);
+}
