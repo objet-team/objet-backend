dcf308b48ce5e55628c7210df766c0db85cd3c70
yujin <jini1514@soongsil.ac.kr>
2023-09-27 03:46:53 +0900

    feat: 토큰 안 필요한 것 예외처리함

--- src/main/java/com/server/objet/domain/product/ProductController.java
+++ src/main/java/com/server/objet/domain/product/ProductController.java
@@ -4,6 +4,7 @@ import com.server.objet.domain.product.dto.req.ProductInfo;
 import com.server.objet.domain.product.dto.res.MainPageProducts;
 import com.server.objet.domain.product.dto.ProductDetail;
 import com.server.objet.domain.product.dto.res.RegisterProductResult;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,16 +20,19 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/popular")
+    @Operation(summary = "작품 홈 인기순", description = "토큰이 필요하지 않습니다.")
     public MainPageProducts PopularProducts() {
         return productService.getPopularProducts();
     }
 
     @GetMapping("/new")
+    @Operation(summary = "작품 홈 최신순", description = "토큰이 필요하지 않습니다.")
     public MainPageProducts NewProducts() {
         return productService.getNewProducts();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/detail/{id}")
+    @Operation(summary = "작품 상세보기", description = "토큰이 필요하지 않습니다.")
     public ProductDetail getProduct(@PathVariable("id") Long id) {
         return productService.getProductDetail(id);
     }
