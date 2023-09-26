cd8f716f69cbd7cbeb19e6be6e21279182899335
yujin <jini1514@soongsil.ac.kr>
2023-09-26 22:59:52 +0900

    feat: URI

--- src/main/java/com/server/objet/domain/auth/AuthController.java
+++ src/main/java/com/server/objet/domain/auth/AuthController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/auth")
+import static com.server.objet.global.RequestURI.AUTH_URI;
+
+@RequestMapping(AUTH_URI)
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
