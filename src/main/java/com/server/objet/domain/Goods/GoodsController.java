8d5ec33ae030a7b82284977136fc7bd64f2f1919
yujin <jini1514@soongsil.ac.kr>
2023-09-28 01:39:07 +0900

    feat: 굿즈 컨트롤러 일단 주석 쏘링

--- src/main/java/com/server/objet/domain/Goods/GoodsController.java
+++ src/main/java/com/server/objet/domain/Goods/GoodsController.java
@@ -16,28 +16,28 @@ public class GoodsController {
 
     private final GoodsService goodsService;
 
-    @GetMapping("/popular-normal")
-    @Operation(summary = "굿즈 홈 일반 인기굿즈 조회", description = "토큰이 필요하지 않습니다.")
-    public MainPageGoods PopularNormalGoods() {
-        return goodsService.getPopularNormalGoods();
-    }
-
-    @GetMapping("/popular-nft")
-    @Operation(summary = "굿즈 홈 NFT 인기굿즈 조회", description = "토큰이 필요하지 않습니다.")
-    public MainPageGoods PopularNftGoods() {
-        return goodsService.getPopularNftGoods();
-    }
-
-    @GetMapping("/detail/{id}")
-    @Operation(summary = "굿즈 상세보기", description = "토큰이 필요하지 않습니다.")
-    public GoodsDetail getGoods(@PathVariable("id") Long id) {
-        return goodsService.getGoodsDetail(id);
-    }
-
-    @PostMapping("/register/normal")
-    @ResponseBody
-    public RegisterNormalGoodsResult registerGoods(@RequestBody ProductInfo productInfo) {
-        Long artistId = (Long) 1L; // 이 부분은 추후 토큰을 이용해서 유저 ID 를 Resolve 하는 코드로 변경 예정
-        return goodsService.doRegisterNormalGoods(productInfo, artistId);
-    }
+//    @GetMapping("/popular-normal")
+//    @Operation(summary = "굿즈 홈 일반 인기굿즈 조회", description = "토큰이 필요하지 않습니다.")
+//    public MainPageGoods PopularNormalGoods() {
+//        return goodsService.getPopularNormalGoods();
+//    }
+//
+//    @GetMapping("/popular-nft")
+//    @Operation(summary = "굿즈 홈 NFT 인기굿즈 조회", description = "토큰이 필요하지 않습니다.")
+//    public MainPageGoods PopularNftGoods() {
+//        return goodsService.getPopularNftGoods();
+//    }
+//
+//    @GetMapping("/detail/{id}")
+//    @Operation(summary = "굿즈 상세보기", description = "토큰이 필요하지 않습니다.")
+//    public GoodsDetail getGoods(@PathVariable("id") Long id) {
+//        return goodsService.getGoodsDetail(id);
+//    }
+//
+//    @PostMapping("/register/normal")
+//    @ResponseBody
+//    public RegisterNormalGoodsResult registerGoods(@RequestBody ProductInfo productInfo) {
+//        Long artistId = (Long) 1L; // 이 부분은 추후 토큰을 이용해서 유저 ID 를 Resolve 하는 코드로 변경 예정
+//        return goodsService.doRegisterNormalGoods(productInfo, artistId);
+//    }
 }
