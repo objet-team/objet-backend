d5b8bbf0a575564c1a4e1ee5a1b94e81122ebb65
yujin <jini1514@soongsil.ac.kr>
2023-10-10 00:19:36 +0900

    fix: 스웨거 설명 수정

--- src/main/java/com/server/objet/domain/artist/ArtistController.java
+++ src/main/java/com/server/objet/domain/artist/ArtistController.java
@@ -39,14 +39,15 @@ public class ArtistController {
 
 
     @GetMapping("/info/public/{artistId}") //남의 계정
-    @Operation(summary = "타 아티스트 정보 조회", description = "토큰이 필요하지 않습니다. 현재 프로필 이미지는 지원하지 않습니다.")
+    @Operation(summary = "타 아티스트 정보 조회", description = "토큰이 필요하지 않습니다.")
     public ResponseEntity<ArtistInfoResponseDto> Info(@PathVariable Long artistId) {
         return ResponseEntity.ok(artistService.getInfo(artistId));
     }
 
     @PatchMapping("/info")
-    @Operation(summary = "나의 아티스트 정보 수정", description = "토큰이 필요합니다. 현재 프로필 이미지는 지원하지 않습니다. " +
-            "카테고리 enum은 UX_UI, GRAPHIC_DESIGN, VIDEO_AND_MOTION_GRAPHICS, ILLUSTRATION, CHARACTER_DESIGN, PHOTOGRAPHY, CRAFTS, CERAMICS_AND_GLASS 입니다.")
+    @Operation(summary = "아티스트 마이페이지 수정", description = "토큰이 필요합니다.\n\n" +
+            "카테고리 enum은 UX_UI, GRAPHIC_DESIGN, VIDEO_AND_MOTION_GRAPHICS, ILLUSTRATION, CHARACTER_DESIGN, PHOTOGRAPHY, CRAFTS, CERAMICS_AND_GLASS 입니다.\n\n" +
+            "리퀘스트 바디 중에 profilePicUrl의 경우 [1.이미지 수정 안함 -> 기존에 있던 url] [2.이미지 수정 함 -> 새 url] [3.기본 이미지로 -> null 던지기]")
     public ResponseEntity<MyArtistInfoResponseDto> ChangeInfo(@RequestBody @Valid ArtistInfoChangeRequest artistInfoChangeRequest,
                                                               @AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseEntity.ok(artistService.changeMyInfo(artistInfoChangeRequest, userDetails));
--- src/main/java/com/server/objet/domain/auth/AuthController.java
+++ src/main/java/com/server/objet/domain/auth/AuthController.java
@@ -29,13 +29,14 @@ public class AuthController {
     }
 
     @GetMapping("/user/info")
-    @Operation(summary = "유저 마이페이지 정보", description = "토큰이 필요합니다. 프로필 이미지 제공합니다.")
+    @Operation(summary = "유저 마이페이지 정보 조회", description = "토큰이 필요합니다. 프로필 이미지 제공합니다.")
     public ResponseEntity<MyInfoResponseDto> MyInfo(@AuthenticationPrincipal CustomUserDetails userDetail) {
         return ResponseEntity.ok(authService.getMyInfo(userDetail));
     }
 
     @PatchMapping("/user/info")
-    @Operation(summary = "유저 마이페이지 정보 수정", description = "토큰이 필요합니다. 프로필 이미지 제공합니다.")
+    @Operation(summary = "유저 마이페이지 수정", description = "토큰이 필요합니다. 프로필 이미지 제공합니다.\n\n" +
+            "리퀘스트 바디 중에 profilePicUrl의 경우 [1.이미지 수정 안함 -> 기존에 있던 url] [2.이미지 수정 함 -> 새 url] [3.기본 이미지로 -> null 던지기]\n")
     public ResponseEntity<MyInfoResponseDto> ChangeInfo(@AuthenticationPrincipal CustomUserDetails userDetail,
                                                         @RequestBody MyInfoChangeRequestDto myInfoChangeRequestDto) {
         return ResponseEntity.ok(authService.changeInfo(userDetail, myInfoChangeRequestDto));
